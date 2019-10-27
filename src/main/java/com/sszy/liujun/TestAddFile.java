5c41ad8c62721e8f9e8e3ec0b1df6f90f6a459c4
sszy1314 <liujun_xin@163.com>
2019-10-27 21:38:48 +0800

    添加 TestAddFile.java

---
+++ src/main/java/com/sszy/liujun/TestAddFile.java
@@ -0,0 +1,27 @@
+package com.sszy.liujun;
+
+/**
+ * @author liujun
+ *
+ */
+public class TestAddFile {
+	private String name;
+	private String age ;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "TestAddFile [name=" + name + ", age=" + age + "]";
+	}
+	
+}
