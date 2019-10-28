01e667897447323735072faa6055af972bda002e
sszy1314 <liujun_xin@163.com>
2019-10-29 00:06:24 +0800

    测试fetch

--- src/main/java/com/sszy/liujun/TestGit.java
+++ src/main/java/com/sszy/liujun/TestGit.java
@@ -5,6 +5,9 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("1 to insert!");
 		System.out.println("2 to insert!");
+		
+		
+		//测试fetch
 		System.out.println("test testBranch!");
 		System.out.println("1 to insert!");
 	}
