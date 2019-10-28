d0a133381c436d18616974e44008be7d87c5d155
sszy1314 <liujun_xin@163.com>
2019-10-29 00:35:31 +0800

    hahah

--- src/main/java/com/sszy/liujun/TestGit.java
+++ src/main/java/com/sszy/liujun/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("1 to insert!");
 		System.out.println("2 to insert!");
-		
+		//hasda
 		
 		//测试fetch
 		System.out.println("test testBranch!");
